1e1f4b7a456faad97ba7ade31e91adea03a4633e
murphy <mola2333@gmail.com>
2017-03-06 19:39:16 +0800

    highlight js add

--- api/ssh_api.py
+++ api/ssh_api.py
@@ -10,7 +10,8 @@ def ssh_cmd(host,commands):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(host, host_obj.ssh_port, host_obj.ssh_user,host_obj.ssh_password)
-    stdin, stdout, stderr = ssh.exec_command(commands,timeout=20)
+    stdin, stdout, stderr = ssh.exec_command(commands,timeout=60)
     res = stdout.readlines()
     ssh.close()
     return res
+
