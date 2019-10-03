24de876abf11b74a7599aad74b2f854e9fa40fee
wuhfen <whf2chen@gmail.com>
2019-10-03 10:18:29 +0800

    修复临时bug

--- gitfabu/tasks.py
+++ gitfabu/tasks.py
@@ -1102,7 +1102,15 @@ def git_batch_update_task(myid,platform="现金网",memos=None):
         updata = change_version_old_to_new(key,platform,classify,method,value)
         data = git_deploy.objects.get(name=key,platform=platform,classify=classify,isops=True,islog=True)
         MyWeb = git_moneyweb_deploy(data.id)
-        export_reslut = MyWeb.export_git(what=method,branch="master",reversion=value) #需要判断是不是vue蛮牛或者现金网,pc和手机都要更新,但是这里先不处理
+        if platform == "现金网":
+            if method = 'web':
+                export_reslut = MyWeb.export_git(what='js_pc',branch=updata.js_pc_branches,reversion=updata.js_pc_release)
+                export_reslut = MyWeb.export_git(what=method,branch="master",reversion=value)
+            else:
+                export_reslut = MyWeb.export_git(what='web',branch="master",reversion=updata.web_release)
+                export_reslut = MyWeb.export_git(what=method,branch="master",reversion=value)
+        else:
+            export_reslut = MyWeb.export_git(what=method,branch="master",reversion=value) #需要判断是不是vue蛮牛或者现金网,pc和手机都要更新,但是这里先不处理
         if export_reslut:
             MyWeb.update_release()
             MyWeb.merge_git()
